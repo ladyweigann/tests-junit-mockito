669f87d93498d12bf7d35df77f76565f677b93b5
Larissa Labanca <ladyweigann@gmail.com>
2023-04-05 22:56:34 -0300

    dto class for user entity

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package br.com.dicasdeumdev.api.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}

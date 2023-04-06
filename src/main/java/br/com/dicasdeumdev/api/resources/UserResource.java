ce55d7293f4985fe629ea38637f07ce38b9eaca9
Larissa Labanca <ladyweigann@gmail.com>
2023-04-05 23:00:00 -0300

    using model mapper as bean and returning dto instead of an entity

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,7 +1,9 @@
 package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,11 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(userService.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
     }
 }
